5f5b3220217a4e730f729181ec8109cdb15b7d17
xuyang <xuyang5188@outlook.com>
2021-05-17 08:55:43 +0800

    数据类型

---
+++ src/main/scala/lesson4/ConsoleLearn.scala
@@ -0,0 +1,14 @@
+package lesson4
+
+import scala.io.StdIn
+
+object ConsoleLearn {
+  def main(args: Array[String]): Unit = {
+    println("请输入你的姓名")
+    val name = StdIn.readLine()
+    println("请输入你的年龄")
+    val age = StdIn.readInt()
+    println(s"欢迎${age}岁的${name}来到")
+  }
+
+}
---
+++ src/main/scala/lesson4/Zifuchuan.scala
@@ -0,0 +1,44 @@
+package lesson4
+
+object Zifuchuan {
+
+  def main(args: Array[String]): Unit = {
+    val name = "wangwen"
+    val age = 12
+
+    // 普通加法字符串
+    println("学生" + name + "，已经" + age + "岁了")
+
+    // 占位符
+    printf("学生%s，已经%d岁了\n", name, age)
+
+    // 类似于shell的那种
+    println(s"学生${name}，已经${age}岁了")
+
+    // 对小数的截取
+    val num = 12.6489
+    println(f"数字截取小数点两位是${num}%40.2f")
+    // 这里的40指的是整个数字的长度，若实际上整个数据长度没有40的话，则会在前面补上空格
+    // 假设实际的数据长度，大于40的话，不要有任何担心。会正常显示
+    // 2指的是保留的小数，这里直接自动四舍五入了
+
+    // 无视一切符号的输出
+    println(raw"数字截取小数点两位是${num}%40.2f")
+
+    // 三引号输出，一般用于sql的编写上
+    val sql =
+      s"""
+         |select *
+         |from student
+         |where 1=1
+         |and name = ${name}
+         |and age = ${age}
+         |
+         |""".stripMargin
+
+    println(sql)
+
+
+  }
+
+}
